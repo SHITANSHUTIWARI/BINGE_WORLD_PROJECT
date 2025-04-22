90cc85aa927b096c54664144324dcbc18f2aca51
Shitanshu Tiwari <shitanshutiwari@Shitanshus-MacBook-Pro-3.local>
2025-04-22 14:44:31 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './Components/Header'
+// import Header from './Components/Header'
 import Slider from './Components/Slider'
 import ProductionHouse from './Components/ProductionHouse'
 import GenreMovieList from './Components/GenreMovieList'
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="">
-        <Header />
+        {/* <Header /> */}
         <Slider />
         <ProductionHouse />
         <GenreMovieList />
